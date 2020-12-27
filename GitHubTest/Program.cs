c70d5f7cfe3b773bfc6a1f413172cfb172b88a70
Mein-PC <Mein-PC@DESKTOP-DFNSR2N>
2020-12-27 13:06:47 +0100

    Projektdateien hinzufügen.

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Test();
+        }
+
+        static void Test()
+        {
+            Console.WriteLine("This is a test programm");
+            int x = 5 - 3;
+            Console.WriteLine("5 - 2 = " + x);
+        }
+    }
+}
