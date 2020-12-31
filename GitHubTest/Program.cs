010bfad4e72f2963ef9d1e942bd8fffb8abc332e
Jörg@FirmenPC <jsalzmann@gmx.de>
2020-12-31 16:59:55 +0100

    Firmen-PC

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -24,6 +24,7 @@ namespace GitHubTest
             Console.WriteLine("Hier wird weiterentwicklet4");
             Console.WriteLine("Hier wird weiterentwicklet5");
 
+            //Firmen-PC
             Tauschen(); 
         }
 
