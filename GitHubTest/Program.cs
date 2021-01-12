e216837c3eeb37be23952cdef3364f15d0ae0d66
Jörg@HomePC <joerg_salzmann@gmx.de>
2021-01-12 18:57:33 +0100

    RELEASE 21 R2.1

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -10,6 +10,7 @@ namespace GitHubTest
             Console.WriteLine("Änderung 1");
             Console.WriteLine("Änderung 2");
             Console.WriteLine("Änderung 3");
+            Console.WriteLine("RELEASE 21 R2.1");
         }
     }
 }
